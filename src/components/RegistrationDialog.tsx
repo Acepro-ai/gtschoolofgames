b6d232b1977c67179cfb2d098e8bf59be5f097d4
AbdulWahab Lawal <143225147+Acepro-ai@users.noreply.github.com>
2025-06-13 16:00:25 +0100

    Update RegistrationDialog.tsx

--- src/components/RegistrationDialog.tsx
+++ src/components/RegistrationDialog.tsx
@@ -1,12 +1,4 @@
-
 import React from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
 interface RegistrationDialogProps {
@@ -16,39 +8,25 @@ interface RegistrationDialogProps {
   buttonText: string;
 }
 
-const RegistrationDialog = ({ 
-  triggerClassName, 
-  variant = "outline", 
+const RegistrationDialog = ({
+  triggerClassName,
+  variant = "outline",
   size = "lg",
-  buttonText 
+  buttonText,
 }: RegistrationDialogProps) => {
+  const handleRedirect = () => {
+    window.open("https://live.zoho.com/bvci-xrq-fyi", "_blank");
+  };
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button 
-          variant={variant} 
-          size={size} 
-          className={triggerClassName}
-        >
-          {buttonText}
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[360px] max-h-[240px] overflow-hidden">
-        <DialogHeader>
-          <DialogTitle className="text-center">Register for Free Masterclass</DialogTitle>
-        </DialogHeader>
-        <div className="h-[70vh] overflow-hidden">
-          <iframe 
-            width="100%" 
-            height="100%" 
-            src="https://webinar.zoho.com/meeting/register/embed?sessionId=1092661401" 
-            frameBorder="0"
-            className="rounded-md"
-            title="Zoho Webinar Registration"
-          ></iframe>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <Button
+      variant={variant}
+      size={size}
+      className={triggerClassName}
+      onClick={handleRedirect}
+    >
+      {buttonText}
+    </Button>
   );
 };
 
